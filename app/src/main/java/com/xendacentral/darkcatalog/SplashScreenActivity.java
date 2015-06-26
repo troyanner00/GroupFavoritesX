96394ffc7f48bf9f437e73f5f6be8b4df1b16a14
Angel Velasquez <angel@xendacentral.com>
2015-06-25 19:26:12 -0500

    First Commit

---
+++ app/src/main/java/com/xendacentral/darkcatalog/SplashScreenActivity.java
@@ -0,0 +1,64 @@
+package com.xendacentral.darkcatalog;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreenActivity extends ActionBarActivity {
+  final static int SPLASH_OUT_TIMEOUT = 3000;
+  final static int SLEEP_INTERVAL = 100;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+    final int welcomeScreenDisplay = SPLASH_OUT_TIMEOUT;
+
+    Thread splashThread = new Thread() {
+      int wait = 0;
+      @Override
+      public void run() {
+        try {
+          super.run();
+          while(wait < SPLASH_OUT_TIMEOUT) {
+            sleep(SLEEP_INTERVAL);
+            wait += SLEEP_INTERVAL;
+          }
+
+        } catch (Exception e) {
+          System.out.println("EXc=" + e);
+
+        } finally {
+          startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+          finish();
+        }
+      }
+    };
+    splashThread.start();
+
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
